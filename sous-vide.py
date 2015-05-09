45acc01f4a632e8654c407d8f2d49fbcf12c918e
notnew <fake@fake.com>
2015-05-09 16:51:38 -0500

    add method to change pin direction
    unexport pin if __init__ fails

--- sous-vide.py
+++ sous-vide.py
@@ -8,19 +8,25 @@ class gpio():
     def __init__(self, pin, direction="in"):
         self.pin = int(pin)
 
-        if direction != "in" and direction != "out":
-            fmt_msg = 'direction must be "in" or "out" ("{}" given'
-            raise ValueError(fmt_msg.format(direction))
-        self.direction = direction
-
         f = open("/sys/class/gpio/export", "w")
         f.write(str(self.pin))
         f.close()
         # wait a bit so the sysfs files can be created and become writeable
         time.sleep(0.2)
 
-        sysfs_dir = "/sys/class/gpio/gpio{}".format(self.pin)
+        try:
+            self.set_direction(direction)
+        except:
+            self.unexport()
+            raise
 
+    def set_direction(self, direction):
+        if direction != "in" and direction != "out":
+            msg = 'direction must be "in" or "out"'
+            raise ValueError(direction, msg)
+        self.direction = direction
+
+        sysfs_dir = "/sys/class/gpio/gpio{}".format(self.pin)
         dir_file = open(os.path.join(sysfs_dir, "direction"), "w")
         dir_file.write(direction)
         dir_file.close()
@@ -28,8 +34,6 @@ class gpio():
         value_path = os.path.join(sysfs_dir, "value")
         value_mode = "w" if direction == "out" else "r"
         self.__value_file__ = open(value_path, value_mode)
-
-        # print(os.path.join(sysfs_dir, "value"), self.__value_file__)
 
     def set(self, value):
         if self.direction == "out":
@@ -39,17 +43,21 @@ class gpio():
         else:
             print("Cannot set input pin ({})".format(self.pin))
 
-    def close(self):
-        # self.__value_file__.close()
+    def unexport(self):
         f = open("/sys/class/gpio/unexport", "w")
         f.write(str(self.pin))
         f.close()
 
+    def close(self):
+        self.__value_file__.close()
+        self.unexport()
+
 
 if __name__ == "__main__":
     print("hello")
+    pin = gpio(18)
     try:
-        pin = gpio(18, direction="out")
+        pin.set_direction("in")
         pin.set(True)
         time.sleep(0.5)
         pin.set(False)
