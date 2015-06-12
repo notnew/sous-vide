f5fdae20b8785a7ffdcd995216c4041d6dcd96af
notnew <fake@fake.com>
2015-06-12 14:27:35 -0500

    gpio: use `with` statement, Context Manager Types
    use `with` statements for files
    define __enter__ and __exit__ to make gpio a Context Manager
    add code for running module as script

--- gpio.py
+++ gpio.py
@@ -6,39 +6,44 @@ class PinInUseException(Exception):
 
 class gpio():
     def __init__(self, pin, direction="in"):
-        self.pin = int(pin)
+        self._pin = int(pin)
+        self._direction = direction
 
+    def __enter__(self):
         try:
-            f = open("/sys/class/gpio/export", "w")
-            f.write(str(self.pin))
-            f.close()
+            with open("/sys/class/gpio/export", "w") as export:
+                export.write(str(self._pin))
+            try:
+                time.sleep(0.2)
+                self.set_direction(self._direction)
+                return self
+            except:
+                self.__exit__()
+                raise
+
         except IOError as err:
-            # if call writing the pin number to /sys/class/gpio/export fails
-            # with error "Device or resource busy" (errno 16) then the pin
-            # is already being used -- already exported or being used by device
-            # tree
-            # Fail, instead of trying to use it again
+            print("matched outer")
             (errno, msg) = err.args
             if errno == 16:
-                raise PinInUseException(self.pin) from err
+                # if call writing the pin number to /sys/class/gpio/export
+                # fails with error "Device or resource busy" (errno 16) then
+                # the pin is already being used -- already exported or being
+                # used by device tree. Fail, instead of trying to reuse it
+                raise PinInUseException(self._pin) from err
             raise
 
-        # wait a bit so the sysfs files can be created and become writeable
-        time.sleep(0.2)
-
-        try:
-            self.set_direction(direction)
-        except:
-            self._unexport()
-            raise
+    def __exit__(self, exc_type=None, exc_val=None, exc_tb=None):
+        with open("/sys/class/gpio/unexport", "w") as unexport:
+            unexport.write(str(self._pin))
+        return False
 
     def set_direction(self, direction):
         if direction != "in" and direction != "out":
             msg = 'direction must be "in" or "out"'
             raise ValueError(direction, msg)
-        self.direction = direction
+        self._direction = direction
 
-        sysfs_dir = "/sys/class/gpio/gpio{}".format(self.pin)
+        sysfs_dir = "/sys/class/gpio/gpio{}".format(self._pin)
         with open(os.path.join(sysfs_dir, "direction"), "w") as dir_file:
             dir_file.write(direction)
 
@@ -47,11 +52,11 @@ class gpio():
         self.__value_fd = os.open(value_path, value_mode)
 
     def set(self, value):
-        if self.direction == "out":
+        if self._direction == "out":
             os.lseek(self.__value_fd, 0, os.SEEK_SET)
             os.write(self.__value_fd, b"1" if value else b"0")
         else:
-            print("Cannot set input pin ({})".format(self.pin))
+            print("Cannot set input pin ({})".format(self._pin))
 
     def get(self):
         os.lseek(self.__value_fd, 0, os.SEEK_SET)
@@ -61,17 +66,25 @@ class gpio():
         elif value == b"0":
             return False
         else:
-            msg = "impossible value from pin {}".format(self.pin)
+            msg = "impossible value from pin {}".format(self._pin)
             raise ValueError(value, msg)
-
-    def _unexport(self):
-        f = open("/sys/class/gpio/unexport", "w")
-        f.write(str(self.pin))
-        f.close()
 
     def close(self):
         try:
             os.close(self.__value_fd)
         finally:
-            self._unexport()
+            self.__exit__()
+
+if __name__ == "__main__":
+    (red,green,blue) = (18,27,22)
+    time.sleep(4)
+    with gpio(red, "out") as r:
+        r.set(True)
+        time.sleep(4)
+        with gpio(green, "out") as g:
+            r.set(False)
+            g.set(True)
+            time.sleep(3)
+            r.set(True)
+            time.sleep(10)
 
