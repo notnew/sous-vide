a880e2ab251c732a34d8f0fde877f97d2beba682
notnew <fake@fake.com>
2015-06-22 15:25:09 -0500

    refactor graph()
    try to make it clearer and cleaner
    add some comments
    break up parts a bit

--- site/js/cooker.js
+++ site/js/cooker.js
@@ -85,43 +85,53 @@ var setHistory = function(history) {
 
 var graph = function (history, i) {
   var valueName = d3.select(this).attr("name");
-  var sampleTime = function (sample) { return sample.sample_time * 1000 };
   var getValue = function (sample) { return sample[valueName]; };
+  var sampleTime = function (sample) { return sample.sample_time * 1000; };
 
-  var start_time = sampleTime(history[0]);
-  var end_time = sampleTime(history[history.length - 1]);
-  var timescale = d3.time.scale()
+  var makeTimeScale = function () {
+    var start_time = sampleTime(history[0]);
+    var end_time = sampleTime(history[history.length - 1]);
+    return d3.time.scale()
       .range([0,1])
       .domain([start_time, end_time]);
+  };
 
-  var scale;
-  if (valueName === "setting") {
-    scale = d3.scale.linear().range([1, 0]);
-  }
-  else {
-    var min, max;
-    if (valueName === "error") {
-      min = d3.min(history, getValue);
-      max = d3.max(history, getValue);
+  var makeYScale = function () {
+    if (valueName === "setting") {
+      return d3.scale.linear().range([1, 0]);
     }
-    else {
-      min = d3.min(history, function (sample) {
+    else if (valueName === "error") {
+      var min = d3.min(history, getValue);
+      var max = d3.max(history, getValue);
+      var padding = (max - min) * 0.05;
+
+      return d3.scale.linear()
+        .domain([min-padding, max+padding])
+        .range([1,0]);
+    }
+    else {     // target or temperature; use the same scale for both
+      var min = d3.min(history, function (sample) {
         return Math.min(sample.temperature, sample.target); });
-      max = d3.max(history, function (sample) {
+      var max = d3.max(history, function (sample) {
         return Math.max(sample.temperature, sample.target); });
+      var padding = (max - min) * 0.05;
+
+      return d3.scale.linear()
+        .domain([min-padding, max+padding])
+        .range([1,0]);
     }
-    var padding = (max - min) * 0.05;
-    scale = d3.scale.linear()
-      .domain([min-padding, max+padding])
-      .range([1,0]);
   }
 
+  var timescale = makeTimeScale(history);
+  var scale = makeYScale();
+
+  // plot the graph
   var timeline = d3.svg.line()
       .x( function(sample) { return timescale(sampleTime(sample)) })
       .y( function(sample) { return scale(getValue(sample)) });
-
   d3.select(this).attr("d", timeline);
 
+  // label graph's axes
   var xAxis = d3.svg.axis()
       .scale(timescale)
       .ticks(4)
@@ -147,7 +157,6 @@ var graph = function (history, i) {
    .selectAll("text")
     .attr({"x": 0, "y": 0, "dy": 0, "dx": 2,
            "transform": "scale(0.3,1) translate(0, 0) scale(0.01)"});
-
 }
 
 disableInputs();  // set tabIndex=-1 for inputs (tab won't focus to input)
